db7f3ff66b472c25d28caa6d3111592450a375fe
TpaeFawzen <30316430+GH-TpaeFawzen@users.noreply.github.com>
2020-09-19 15:54:24 +0900

    replacing two

--- slashes-outputs-when-done.sh
+++ slashes-outputs-when-done.sh
@@ -6,6 +6,23 @@ export PATH="$(command -p getconf PATH 2>/dev/null)${PATH+:}${PATH-}"
 case $PATH in :*) PATH=${PATH#?};; esac
 export UNIX_STD=2003
 
+while :; do
+	case $# in 0) break; esac
+	case $1 in
+		'-d')
+			debug=1
+			shift
+			;;
+		'-d'[12])
+			debug=${1#-d}
+			shift
+			;;
+		*)
+			break
+			;;
+	esac
+done
+
 sedlyLF="$(printf '\\\012_')"; sedlyLF="${sedlyLF%_}"
 {
 	od -A n -t x1 -v "${1:--}" |
@@ -17,16 +34,42 @@ sedlyLF="$(printf '\\\012_')"; sedlyLF="${sedlyLF%_}"
 	echo
 } |
 awk '
+BEGIN{
+	debug="'"${debug:-NOPE}"'";
+	for(i=0;i<=255;i++)
+		daHex2Oct[\
+			sprintf("%02x",i)\
+		]=sprintf("%03o",i);
+}
+function myPrint(\
+	argStr,\
+	localA,\
+	localN,\
+	localI\
+){
+	localN=split(argStr,localA,",")-1;
+	for(localI=1;localI<=localN;localI++){
+		print daHex2Oct[localA[localI]];
+	}
+}
 {
 	for(;;){
-		if(match($0,/^(2[^f],|5[^c],|[^25].,)+/)){
-			printf substr($0,1,RLENGTH);
+		if(debug~1)
+			printf"\n[%s]\n",$0 >"/dev/stderr";
+		if(match($0,"^(2[^f],|5[^c],|[^25].,)+")){
+			# printf substr($0,1,RLENGTH);
+			myPrint(substr($0,1,RLENGTH));
 			$0=substr($0,RLENGTH+1);
+			if(debug~2)
+				printf"\n[%s]\n",$0 >"/dev/stderr";
 			continue;
 		}
-		if(match($0,/^5c,..,/)){
-			printf substr($0,1+3,3);
-			$0=substr($0,RLENGTH+1);
+		if(/^5c,..,/){
+			# printf substr($0,1+3,3);
+			myPrint(substr($0,1+3,3));
+			$0=substr($0,6+1);
+			if(debug~2)
+				printf"\n[%s]\n",$0 >"/dev/stderr";
 			continue;
 		}
 		if(match($0,\
@@ -34,26 +77,23 @@ awk '
 			"(2[^f],|5[^c],|[^25].,|5c,..,)*2f,"\
 			"(2[^f],|5[^c],|[^25].,|5c,..,)*2f,")\
 		){
-			rEndAt=RLENGTH;
-			match($0,"^2f,(2[^f],|5[^c],|[^25].,|5c,..,)*2f");
-			pEndAt=RLENGTH;
 			oneCharLen=3;
-			rBeginAt=pEndAt+oneCharLen-1;
-			pBeginAt=1+oneCharLen;
-			s=substr($0,pBeginAt,pEndAt-pBeginAt-oneCharLen+2);
-			d=substr($0,rBeginAt,rEndAt-rBeginAt-oneCharLen+1);
-
-			programContinueFrom=rEndAt+1;
-			$0=substr($0,programContinueFrom);
+			patternFirst=1+oneCharLen;
+			replaceLast=RLENGTH-oneCharLen;
+			match($0,"^2f,(2[^f],|5[^c],|[^25].,|5c,..,)*2f,");
+			patternLast=RLENGTH-oneCharLen;
+			replaceFirst=RLENGTH+1;
+			s=substr($0,patternFirst,patternLast-patternFirst+1);
+			d=substr($0,replaceFirst,replaceLast-replaceFirst+1);
+			$0=substr($0,replaceLast+oneCharLen+1);
 
 			# You know what, many impls of utils with ERE
 			# might not support what looks like /(..)\1/ 
-			for(;escPos=match(s,/5c,...,/);)
-				s=substr(s,1,escPos-3) substr(s,escPos+3);
-			for(;escPos=match(d,/5c,...,/);)
-				d=substr(d,1,escPos-3) substr(d,escPos+3);
+			# SO LET THE SED DO SOMETHING WITH IT!
+			sprintf("echo %s | sed \"s/5c,\\(..,\\)/\\1/g\"",s) | getline s;
+			sprintf("echo %s | sed \"s/5c,\\(..,\\)/\\1/g\"",d) | getline d;
 
-			# print s,d>"/dev/stderr";
+			# print s,d,$0>"/dev/stderr";
 
 			for(;$0~s;)
 				sub(s,d);
@@ -63,7 +103,6 @@ awk '
 		break;
 	}
 }' | 
-tr , '\n' |
 awk '
 BEGIN{
 	for(i=1;i<256;i++){
@@ -119,9 +158,7 @@ BEGIN{
 END{
 	if(NR) print LF;
 	else printf"\"\"\n";
-}
-
-' |
+}' |
 xargs -n 1 printf
 
 # finally
