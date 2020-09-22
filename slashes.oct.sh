99762b0b8636071667001382b616415b83866fee
TpaeFawzen <30316430+GH-TpaeFawzen@users.noreply.github.com>
2020-09-22 10:33:43 +0900

    erased dead code

--- slashes.oct.sh
+++ slashes.oct.sh
@@ -30,7 +30,6 @@ case $# in
 	esac
 esac
 
-sedlyLF="$(printf '\\\012_')"; sedlyLF="${sedlyLF%_}"
 {
 	od -A n -t o1 -v "${1:--}" |
 		tr -Cd '01234567'
@@ -92,58 +91,6 @@ function action(){
 		break;
 	}
 	if(!mode&&what2print!~/^$/)myPrint(what2print);
-}
-0{
-	for(;;){
-		if(debug~1)
-			printf"\n[%s]\n",$0 >"/dev/stderr";
-		if(match($0,"^(2[^f],|5[^c],|[^25].,)+")){
-			# printf substr($0,1,RLENGTH);
-			myPrint(substr($0,1,RLENGTH));
-			$0=substr($0,RLENGTH+1);
-			if(debug~2)
-				printf"\n[%s]\n",$0 >"/dev/stderr";
-			continue;
-		}
-		if(/^5c,..,/){
-			# printf substr($0,1+3,3);
-			myPrint(substr($0,1+3,3));
-			$0=substr($0,6+1);
-			if(debug~2)
-				printf"\n[%s]\n",$0 >"/dev/stderr";
-			continue;
-		}
-		if(match($0,\
-			"^2f,"\
-			"(2[^f],|5[^c],|[^25].,|5c,..,)*2f,"\
-			"(2[^f],|5[^c],|[^25].,|5c,..,)*2f,")\
-		){
-			oneCharLen=3;
-			patternFirst=1+oneCharLen;
-			replaceLast=RLENGTH-oneCharLen;
-			match($0,"^2f,(2[^f],|5[^c],|[^25].,|5c,..,)*2f,");
-			patternLast=RLENGTH-oneCharLen;
-			replaceFirst=RLENGTH+1;
-			s=substr($0,patternFirst,patternLast-patternFirst+1);
-			d=substr($0,replaceFirst,replaceLast-replaceFirst+1);
-			$0=substr($0,replaceLast+oneCharLen+1);
-
-			# You know what, many impls of utils with ERE
-			# might not support what looks like /(..)\1/ 
-			# SO LET THE SED DO SOMETHING WITH IT!
-			template="echo %s | sed \"s/5c,\\(..,\\)/\\1/g\"";
-			sprintf(template,s) | getline s;
-			sprintf(template,d) | getline d;
-
-			# print s,d,$0>"/dev/stderr";
-
-			for(;$0~s;)
-				sub(s,d);
-
-			continue;
-		}
-		break;
-	}
 }' |
 sed s/.../\\\\&/g |
 xargs -n 1 printf 2>/dev/null
